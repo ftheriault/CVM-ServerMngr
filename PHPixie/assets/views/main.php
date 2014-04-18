c9871dabcc9be238fda4acf36584125da2713567
Frederic Theriault <fred.theriault@gmail.com>
2014-04-18 10:15:22 -0500

    Added first layout logic with PHPixie framework

---
+++ PHPixie/assets/views/main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CVM Server Manager</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet/less" href="/css/global.less" />
+		<script src="/js/less.js"></script>
+	</head>
+	<body>
+		<div class="band">
+			<h5>CVM Server Manager</h5>
+		</div>
+		
+		<div class="main-section">
+			<div class="container">
+				<?php include($subview.'.php');?>	
+			</div>	
+		</div>
+		
+		<div class="band footer">
+			Made with passion, by Frédéric Thériault
+		</div>
+	</body>
+</html>
